b5b788e095e369c5156098b1c5ea97a4099f8ea5
nidedno <nidedno@gmail.com>
2025-01-26 17:19:02 +0500

    Add save for accounts

--- core/window/tabs/account_tab.py
+++ core/window/tabs/account_tab.py
@@ -1,7 +1,11 @@
+import glob
+import json
+import os
 import dearpygui.dearpygui as dpg
 from ...shared.utils import import_accounts, read_json
-from ...shared.constants import SETTINGS_FOLDER
+from ...shared.constants import ACCOUNTS_FOLDER, SETTINGS_FOLDER
 from ...logic.account import Account
+from ..GLOBAL import GLOBAL_STATE
 
 TAG_ACCOUNTS = "Accounts"
 TAG_ACCOUNTS_TABLE = "Accounts_Table"
@@ -20,7 +24,7 @@ def create_accounts_tab():
             dpg.add_text("Accounts")
             dpg.add_button(label= "+", tag=TAG_ACCOUNTS_ADD, callback=create_account)
             dpg.add_button(label="refresh", tag=TAG_ACCOUNTS_REFRESH, callback=refresh_table)
-            dpg.add_button(label="save", tag=TAG_ACCOUNTS_SAVE)
+            dpg.add_button(label="save", tag=TAG_ACCOUNTS_SAVE, callback=save_table, user_data=TAG_ACCOUNTS_TABLE)
         dpg.add_separator()
 
         # Add table.
@@ -50,7 +54,6 @@ def add_account(account, fields):
             value = "~" # it means that it's empty.
             secret = fields[field]['secret']
             readonly = fields[field]['readonly']
-            print(secret, readonly, field)
             if (account.contains_field(field)):
                 value = account.get(field)
 
@@ -72,7 +75,6 @@ def edit_value(_, __, user_data):
 def create_account():
     try:
         fields = read_json(SETTINGS_FOLDER + "account.json")
-
         with dpg.window(label="Account creation", modal=True, tag=TAG_ACCOUNT_FORM, show=True, autosize=True, on_close=lambda: dpg.delete_item(TAG_ACCOUNT_FORM)):
             for field in fields.keys():
                 dpg.add_input_text(label=field, tag=field_to_tag(field))
@@ -109,13 +111,62 @@ def save_account():
     
     # close form
     dpg.delete_item(TAG_ACCOUNT_FORM)
+
+    # refresh table
+    refresh_table()
     
 def refresh_table():
     try:
         if dpg.does_item_exist(TAG_ACCOUNTS_TABLE):
             dpg.delete_item(TAG_ACCOUNTS_TABLE)
         accounts = import_accounts()
+        GLOBAL_STATE["accounts"] = accounts
     except Exception as e:
         dpg.add_text("No accounts", e)
     else:
         create_accounts_table(accounts)
+        
+# soft save
+def save_table(__, _, userData):
+    # receive all available fields.
+    fields = list(read_json(SETTINGS_FOLDER + "account.json").keys())
+
+    # get table rows.
+    table_rows_ids = dpg.get_item_children(userData)[1]
+    accounts = []
+    
+    for i in range(len(table_rows_ids)):
+        # get row data.
+        row_id = table_rows_ids[i]
+
+        #create save account
+        account_save_data = {}
+
+        row_inputs_ids = dpg.get_item_children(row_id)[1]
+        # row represented in global state['accounts'] so simply:
+        for j in range(len(row_inputs_ids)):
+            field = fields[j]
+            value = row_inputs_ids[j]
+            account_save_data[field] = dpg.get_value(value)
+
+        accounts.append(account_save_data)
+
+    # Soft update.
+    i = 0
+    for filename in glob.glob(os.path.join(ACCOUNTS_FOLDER, '*.json')):
+        with open(filename, 'r+') as account_file:
+            account_text = account_file.read()
+            account_json = json.loads(account_text)
+
+            for key in accounts[i].keys():
+                if (key in account_json.keys()):
+                    account_json[key] = accounts[i][key]
+                    #print('set', key, account_json[key], '->', accounts[i][key])
+
+            account_file.seek(0)  
+            json.dump(account_json, account_file, indent=6)
+            account_file.truncate()
+        account_file.close()
+        i+=1
+
+    refresh_table()
